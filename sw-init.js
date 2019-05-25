635571f6fa659908c9a2db19c60c5dc8e41e59ed
SD36 <SD36@mitt.ca>
2019-05-25 18:41:39 -0500

    First committed

---
+++ sw-init.js
@@ -0,0 +1,22 @@
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('./sw.js')
+  .then(function() {
+    console.log("Installed Service Worker");
+  })
+
+  .catch(function() {
+    console.log("Didn't installed Service Worker")
+  })
+}
+
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('sw.js')
+//       .then(reg => {
+//         console.log('service worker is registered', reg)
+//       })
+//       .catch(err => {
+//         console.log('service worker is not registered', err)
+//       })
+//   })
+// }
---
+++ sw.js
@@ -0,0 +1,61 @@
+const currentCache = 'myCache';
+
+self.addEventListener('install', function(event) {
+  event.waitUntill(
+    caches.open(currentCache).then(function(newCache) {
+      return newCache.addAll(
+        [
+          './',
+          './index.html',
+          './restaurant.html?id=1',
+          './restaurant.html?id=2',
+          './restaurant.html?id=3',
+          './restaurant.html?id=4',
+          './restaurant.html?id=5',
+          './restaurant.html?id=6',
+          './restaurant.html?id=7',
+          './restaurant.html?id=8',
+          './restaurant.html?id=9',
+          './restaurant.html?id=10',
+          './css/styles.css',
+          './js/dbhelper.js',
+          './js/main.js',
+          './js/restaurant_info.js',
+          './data/restaurants.json',
+          './img/1.jpg',
+          './img/2.jpg',
+          './img/3.jpg',
+          './img/4.jpg',
+          './img/5.jpg',
+          './img/6.jpg',
+          './img/7.jpg',
+          './img/8.jpg',
+          './img/9.jpg',
+          './img/10.jpg'
+        ]
+      )
+    })
+  )
+})
+
+self.addEventListener('activate', function(event) {
+  event.waitUntill(
+    caches.keys().then(function(cachedNames) {
+      return Promise.all(
+        cachedNames.filter(function(cachedName) {
+          return cachedName.startsWith("myCache") && cachedName != currentCache;
+        }).map(function(cachedName) {
+          return caches.delete(cachedName);
+        })
+      )
+    })
+  )
+})
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  )
+})
