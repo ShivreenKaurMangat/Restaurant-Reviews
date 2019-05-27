d67b31a0bf7b3088f62d1a72b55965fc0d11544c
SD36 <SD36@mitt.ca>
2019-05-26 22:13:00 -0500

    commit

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const currentCache = 'myCache';
+const currentCache = 'myCache-v1';
 
 self.addEventListener('install', function(event) {
   event.waitUntill(
@@ -22,16 +22,16 @@ self.addEventListener('install', function(event) {
           './js/main.js',
           './js/restaurant_info.js',
           './data/restaurants.json',
-          'img/1.jpg',
-          'img/2.jpg',
-          'img/3.jpg',
-          'img/4.jpg',
-          'img/5.jpg',
-          'img/6.jpg',
-          'img/7.jpg',
-          'img/8.jpg',
-          'img/9.jpg',
-          'img/10.jpg'
+          './img/1.jpg',
+          './img/2.jpg',
+          './img/3.jpg',
+          './img/4.jpg',
+          './img/5.jpg',
+          './img/6.jpg',
+          './img/7.jpg',
+          './img/8.jpg',
+          './img/9.jpg',
+          './img/10.jpg'
         ]
       )
     })
