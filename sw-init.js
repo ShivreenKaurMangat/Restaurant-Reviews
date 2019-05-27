9a2464caa17723216bb39198a10b79b753937c7d
SD36 <SD36@mitt.ca>
2019-05-26 20:54:27 -0500

    commit

--- sw-init.js
+++ sw-init.js
@@ -8,16 +8,3 @@ if (navigator.serviceWorker) {
     console.log("Didn't installed Service Worker")
   })
 }
-
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('sw.js')
-//       .then(reg => {
-//         console.log('service worker is registered', reg)
-//       })
-//       .catch(err => {
-//         console.log('service worker is not registered', err)
-//       })
-//   })
-// }
-
